6ea4cc518957a94bb89d7a2963e57dc38ec5edd6
sumit arora <sumitarora@sumits-MacBook-Pro.local>
2019-04-21 22:37:34 +0530

    changes for akka plane

--- app/zzz/akka/avionics/Altimeter.scala
+++ app/zzz/akka/avionics/Altimeter.scala
@@ -19,7 +19,8 @@ class Altimeter extends Actor with ActorLogging{
   var rateOfClimb = 0f
   var altitude = 0d
   
-  val ticker = context.system.scheduler.schedule(100.millis, 100.millis, self, Tick)
+  var lastTick = System.currentTimeMillis()
+  val ticker = context.system.scheduler.schedule(100, 100, self, Tick)
   
   case object Tick
   
@@ -29,8 +30,14 @@ class Altimeter extends Actor with ActorLogging{
       rateOfClimb = amount.min(1.0f).max(-1.0f) * maxRateofclimb 
       log info (s"Altimeter changed rate of climb to $rateOfClimb")
       
+    case Tick => 
+      val tick = System.currentTimeMillis()
+      altitude = altitude + (tick-lastTick) / 60000.0 * rateOfClimb
+      lastTick = tick
+      
   }
   
-  
-  
+  // Kill the ticker when we stop
+  override def postStop(): Unit = ticker.cancel
+    
 }
